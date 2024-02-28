db8fcd78b78be6812d8da91b77f57b81927f0848
Hashi1802 <156032305+Hashi1802@users.noreply.github.com>
2024-02-27 21:14:44 -0300

    atualizaçao do javascript

--- exercicios/ex013/script.js
+++ exercicios/ex013/script.js
@@ -1,6 +1,21 @@
-function carregar (){
+function carregar () {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
-var data = new date ()
-var hora = data.getHours()
+var data = new Date()
+var hora = 08 //data.getHours()
+
+msg.innerHTML = `agora são ${hora}hrs`
+if (hora >= 0 && hora < 12) {
+    // BOM DIA!
+    img.src = 'imagens/manha.png' 
+    document.body.style.background = '#FAD2AE'
+} else if (hora >= 12 && hora < 18) {
+    // BOA TARDE!
+    img.src = 'imagens/tarde.png'
+    document.body.style.background = '#FD921E'
+} else {
+    // BOA NOITE!
+    img.src = 'imagens/noite.png'
+    document.body.style.background = '#555D91'
 }
+} 
