f5ed5ae6b929b54b64b76bc8bb71113ef91d44aa
Hashi1802 <156032305+Hashi1802@users.noreply.github.com>
2024-04-12 08:56:44 -0300

    att

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -2,11 +2,20 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtanto')
-    var res = document.getElementById('res')
-    if (fano.value.lenght == 0 || fano.value > ano){window.alert('[ERRO]verifique os dados e tente novamente!')}
-} else { 
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){window.alert('[ERRO]verifique os dados e tente novamente!')
+    } else { 
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
-    res.innerHTML = `idade calculada $(idade)`
+    var genero = ''
+    var img = document.createElement('img')
+    //img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        genero = 'Homem'
+    } else if (fsex[1].checked) {
+        genero = 'mulher'
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
     
 }
