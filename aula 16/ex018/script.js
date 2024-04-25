e23bf9552952eca3801ccf66023b0dc4ee67552a
Hashi1802 <156032305+Hashi1802@users.noreply.github.com>
2024-04-24 21:00:09 -0300

    att

--- aula 16/ex018/script.js
+++ aula 16/ex018/script.js
@@ -21,8 +21,38 @@ function inlista(n, l) {
 function adicionar() {
 if (isnumero(num.value) && !inlista(num.value, valores)) {
     valores.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} adicionado`
+    lista.appendChild(item)
+    res.innerHTML = ''
 } else { 
     window.alert('Valor Invalido ou ja encontrado!')
 }
-
+    num.value = ''
+    num.focus = ()
+}
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores [0]
+        let soma = 0
+        let media =  0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor) 
+            menor = valores[pos]
+        } 
+        media = soma / tot
+        res.innerHTML = '' 
+        res.innerHTML += `<p> ao todo temos ${tot} numeros cadastrados.`
+        res.innerHTML += `<p> o maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p> o menor valor informado foi ${menor}</p> `
+        res.innerHTML += `<p> somando todos os valores, temos ${soma}. </p>`
+        res.innerHTML += `<p> a media dos valores é ${media} </p>`
+    }
 }
